ee1627987d37828eaf4bb9a64d335b64cbc26442
NDG-Remote <andrea.glaus.digital@gmail.com>
2025-02-20 09:44:45 +0100

    Opti: Added more languages

---
+++ src/components/ExampleComponent.jsx
@@ -0,0 +1,15 @@
+const ExampleComponent = () => (
+  <div>className="flex justify-center items-center h-screen">className="flex items-center h-screen">className="flex justify-center">className="container mx-auto">
+    <p className="text-center">This text is centered.</p>
+  </div>>This flex item is centered both horizontally and vertically.</p>>This flex item is centered vertically.</p>>This flex item is centered horizontally.</p> className="text-center">This block element is centered horizontally.</p>
+);  </div>  </div>  </div>  </div>
+  </div>  </div>  </div>  </div>
+export default ExampleComponent;
+
+
+
+export default ExampleComponent;
+
+export default ExampleComponent;
+
+export default ExampleComponent;export default ExampleComponent;
