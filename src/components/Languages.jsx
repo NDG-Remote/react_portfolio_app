6d7870efe61ec78122e22f21e224cd3912421369
NDG-Remote <andrea.glaus.digital@gmail.com>
2025-02-20 09:14:49 +0100

    Feat: Slider added

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -1,16 +1,55 @@
 import { languages } from "../constants";
 import styles from "../style";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Clients = () => (
-  <section className={`${styles.flexCenter} my-4`}>
-    <div className={`${styles.flexCenter} flex-wrap w-full`}>
-      {languages.map((client) => (
-        <div key={client.id} className={`flex-2 ${styles.flexCenter} sm:min-w-[110px] min-w-[100px] m-5`}>
-          <img src={client.logo} alt="languages_logo" className="sm:w-[80px] w-[50px] object-contain icon-gray" />
-        </div>
-      ))}
-    </div>
-  </section>
-);
+const Clients = () => {
+  const settings = {
+
+    infinite: true,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+  return (
+    <section className="mx-6 my-4">
+      <Slider {...settings}>
+        {languages.map((client) => (
+          <div key={client.id} className={`flex-2 ${styles.flexCenter} sm:min-w-[110px] min-w-[100px] m-5`}>
+            <img src={client.logo} alt="languages_logo" className="sm:w-[80px] w-[50px] object-contain icon-gray" />
+          </div>
+        ))}
+      </Slider>
+    </section>
+  );
+};
 
 export default Clients;
